8f0c326117f05887dce3ecad8e93af7aa8ee8ab0
roberta de cecco <roberta.dececco@gmail.com>
2023-11-29 19:17:27 +0800

    added a category field with dropdown options to the form page

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -6,5 +6,4 @@ class NewsStory(models.Model):
     author = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     content = models.TextField()
-
-
+    category = models.CharField(max_length=50, choices=[("dogs", "Dogs"), ("cats", "Cats"), ("dadj", "Dad's Jokes"), ("other", "Other")], blank=True)
